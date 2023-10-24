8af1e32d7c48720b26f7ed3daf24f2b71aa33a22
Avtomatplis <119958728+Avtomatplis@users.noreply.github.com>
2023-10-24 14:47:48 +0300

    Add files via upload

---
+++ Fractal_Biomorf/Fractal_Biomorf/Fractal_Biomorf/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal_Biomorf
+{
+    public partial class Form1 : Form
+    {
+        private const int iter = 70000;
+        private const double a = 2.8;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Random rand = new Random();
+            double x = 0.0;
+            double y = 0.0;
+            double t;
+
+            for (int k = 0; k < iter; k++)
+            {
+                double p = rand.NextDouble();
+                t = x;
+
+                if (p <= 0.5)
+                {
+                    x = -y;
+                    y = t;
+                }
+                else
+                {
+                    x = 1 + (a * (x - 1)) / (Math.Pow(x - 1, 2) + Math.Pow(y, 2) + 1);
+                    y = a * y / (Math.Pow(t - 1, 2) + Math.Pow(y, 2) + 1);
+                }
+
+                e.Graphics.FillRectangle(Brushes.Black, (int)(ClientSize.Width / 2 + 200 * x), (int)(ClientSize.Height / 2 - 200 * y), 1, 1);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Fractal_Stars/Fractal_Stars/Fractal_Stars/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal_Stars
+{
+    public partial class Form1 : Form
+    {
+        private const double it = 1280;//количество итераций для отрисовки звезды
+        private const double r = 0.35;//коэффициент масштабирования
+        private const double l = 300;// начальная длина линии
+        private const double da = 4 * Math.PI / 5; //угол поворота между ветвями звезды(P.S. изменив 4 на 2 или 3 - будет фрактал много-ка)
+        private const double v = 4;//число ветвей звезды
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        protected override void OnPaint(PaintEventArgs e)//рисование фрактала Звезды
+        {
+            base.OnPaint(e);
+
+            double a = 0;
+            double x = 280, y = 330;
+            double xn, yn;
+
+            for (int i = 0; i < it; i++)//цикл до конца кол-ва итераций для отрисовки звезды
+                                        //l-длина линии звезды
+            {
+                xn = x + Math.Sin(a) * l * Mn(i);//высчитываем новые координаты конца линии по x и y каждый раз
+                yn = y - Math.Cos(a) * l * Mn(i);
+
+                e.Graphics.DrawLine(Pens.Blue, (float)x, (float)y, (float)xn, (float)yn);//рисуем линию от текущей координаты x и y до новой
+                                                                                         //координаты
+                                                                                         //и после обновляем координаты
+                x = xn;
+                y = yn;
+                a += da;
+            }
+        }
+
+        private double Mn(int nn)//высчитывание коэфициента масштабирования
+        {
+            if (nn % (v * v * v * v) == 0) return 1;
+            if (nn % (v * v * v) == 0) return r;
+            if (nn % (v * v) == 0) return r * r;
+            if (nn % v == 0) return r * r * r;
+            return r * r * r * r;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
